4f6875b7d41e3c98525541ac347316af63470ce5
namvu2101 <buidung181201@gmail.com>
2023-12-14 05:13:57 +0700

    update

--- firebase.config.js
+++ firebase.config.js
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+// import {firebase} from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCo2k_SyjOSOpxaaiWlCJvEn0CIEl23JCk",
-  authDomain: "qr-atentdance.firebaseapp.com",
-  projectId: "qr-atentdance",
-  storageBucket: "qr-atentdance.appspot.com",
-  messagingSenderId: "285780628611",
-  appId: "1:285780628611:web:51200a989bc0fd535fb7b6",
+  apiKey: 'AIzaSyCo2k_SyjOSOpxaaiWlCJvEn0CIEl23JCk',
+  authDomain: 'qr-atentdance.firebaseapp.com',
+  projectId: 'qr-atentdance',
+  storageBucket: 'qr-atentdance.appspot.com',
+  messagingSenderId: '285780628611',
+  appId: '1:285780628611:web:51200a989bc0fd535fb7b61',
 };
 
 // Initialize Firebase
