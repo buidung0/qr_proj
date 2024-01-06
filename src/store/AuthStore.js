bfe83bc23fbe74605b40c11fa3c5dda183c5b451
namvu2101 <buidung181201@gmail.com>
2024-01-06 10:21:49 +0700

    update store

--- src/store/AuthStore.js
+++ src/store/AuthStore.js
@@ -1,19 +1,11 @@
-import {
-  addDoc,
-  collection,
-  doc,
-  getDocs,
-  query,
-  updateDoc,
-  where,
-} from "firebase/firestore";
-import { fireStore, storage } from "../../firebase.config";
-import { Alert } from "react-native";
-import { DEFAULT_AVT, ROLES } from "../enum/role";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-const { makeAutoObservable } = require("mobx");
-const CryptoJS = require("crypto-js");
+import { addDoc, collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import { fireStore, storage } from '../../firebase.config';
+import { Alert } from 'react-native';
+import { DEFAULT_AVT, ROLES } from '../enum/role';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+const { makeAutoObservable } = require('mobx');
+
 class AuthStore {
   DB = fireStore;
   logged = false;
@@ -22,7 +14,7 @@ class AuthStore {
     logging: false,
   };
   user = {};
-
+  role = {};
   constructor() {
     makeAutoObservable(this);
   }
@@ -31,6 +23,9 @@ class AuthStore {
   };
   setUser = (val) => {
     this.user = val;
+  };
+  setRole = (val) => {
+    this.role = val;
   };
   setLogged = (val) => {
     this.logged = val;
@@ -41,7 +36,7 @@ class AuthStore {
 
   logout = async (deleteCookies) => {
     if (deleteCookies) {
-      await AsyncStorage.removeItem("auth_info");
+      await AsyncStorage.removeItem('auth_info');
     }
     this.setLogged(false);
     this.setUser({});
@@ -53,23 +48,19 @@ class AuthStore {
         this.setLogging(true);
         setTimeout(async () => {
           // const hashedPassword = CryptoJS.SHA256(password).toString();
-          const doc = await getDocs(
-            query(collection(this.DB, "users"), where("email", "==", email))
-          );
-          const doc1 = await getDocs(
-            query(collection(this.DB, "users"), where("msv", "==", msv))
-          );
+          const doc = await getDocs(query(collection(this.DB, 'users'), where('email', '==', email)));
+          const doc1 = await getDocs(query(collection(this.DB, 'users'), where('msv', '==', msv)));
           if (doc.docs.length > 0) {
-            Alert.alert("Error", "Email has been taken");
+            Alert.alert('Error', 'Email has been taken');
             this.setLogging(false);
             return;
           }
           if (doc1.docs.length > 0) {
-            Alert.alert("Error", "Studdent ID has been taken");
+            Alert.alert('Error', 'Studdent ID has been taken');
             this.setLogging(false);
             return;
           }
-          addDoc(collection(this.DB, "users"), {
+          addDoc(collection(this.DB, 'users'), {
             name,
             role: ROLES.STUDENT,
             email,
@@ -78,68 +69,37 @@ class AuthStore {
             msv,
             avatar: DEFAULT_AVT,
           });
-          Alert.alert("Success", "Register success");
+          Alert.alert('Success', 'Register success');
           this.setLogging(false);
         }, 1000);
       }
     } catch (e) {
-      Alert.alert("Serve Error", "Something error please retry later");
+      Alert.alert('Serve Error', 'Something error please retry later');
     }
   };
-  // changePassword = async ({ email, password, newPassword }) => {
-  //   try {
-  //     if (!this.draft.logging) {
-  //       this.setLogging(true);
-  //       setTimeout(async () => {
-  //         const doc = await getDocs(
-  //           query(collection(this.DB, "users"), where("email", "==", email))
-  //         );
-  //         if (doc.docs.length > 0) {
-  //           Alert.alert("Error", "Email has been taken");
-  //           this.setLogging(false);
-  //           return;
-  //         }
-
-  //         addDoc(collection(this.DB, "users"), {
-  //           email,
-  //           password,
-  //           newPassword,
-  //         });
-  //         Alert.alert("Success", "Register success");
-  //         this.setLogging(false);
-  //       }, 1000);
-  //     }
-  //   } catch (e) {
-  //     Alert.alert("Serve Error", "Something error please retry later");
-  //   }
-  // };
   login = async ({ email, password }) => {
     try {
       console.log(email, password);
       if (!this.draft.logging) {
         this.setLogging(true);
         setTimeout(async () => {
-          const q = query(
-            collection(this.DB, "users"),
-            where("email", "==", email),
-            where("password", "==", password)
-          );
+          const q = query(collection(this.DB, 'users'), where('email', '==', email), where('password', '==', password));
           const queryResult = await getDocs(q);
           if (queryResult.size > 0) {
             queryResult.forEach((us) => {
               const us_ = { ...us.data(), id: us.id };
               this.setUser(us_);
             });
-            await AsyncStorage.setItem("auth_info", JSON.stringify(this.user));
+            await AsyncStorage.setItem('auth_info', JSON.stringify(this.user));
             this.setLogged(true);
           } else {
-            Alert.alert("Login Error", "Email or password incorrect");
+            Alert.alert('Login Error', 'Email or password incorrect');
           }
           this.setLogging(false);
         }, 100);
       }
     } catch (e) {
-      Alert.alert("Serve Error", "Something error please retry later");
+      Alert.alert('Serve Error', 'Something error please retry later');
     }
   };
 
@@ -149,58 +109,110 @@ class AuthStore {
         this.setLogging(true);
         setTimeout(async () => {
           const q = query(
-            collection(this.DB, "users"),
-            where("email", "==", email),
-            where("password", "==", oldPassword)
+            collection(this.DB, 'users'),
+            where('email', '==', email),
+            where('password', '==', oldPassword),
           );
           const queryResult = await getDocs(q);
           if (queryResult.size > 0) {
             queryResult.forEach(async (us) => {
               // Assuming only one user matches the email and old password
-              const userRef = doc(this.DB, "users", us.id);
-  
+              const userRef = doc(this.DB, 'users', us.id);
               // Update password in Firestore
               await updateDoc(userRef, { password: newPassword });
-  
               // You might want to update the local user object if needed
               const updatedUser = { ...us.data(), password: newPassword };
               this.setUser(updatedUser);
-  
               // Optionally, you can also update the AsyncStorage with new auth info
-              await AsyncStorage.setItem("auth_info", JSON.stringify(updatedUser));
-  
+              await AsyncStorage.setItem('auth_info', JSON.stringify(updatedUser));
               // Inform the user about the successful password change
-              Alert.alert(
-                "Password Changed",
-                "Your password has been updated successfully"
-              );
+              Alert.alert('Password Changed', 'Your password has been updated successfully');
             });
           } else {
             // Alert if the email or old password is incorrect
-            Alert.alert(
-              "Change Password Error",
-              "Email or old password incorrect"
-            );
+            Alert.alert('Change Password Error', 'Email or old password incorrect');
           }
           this.setLogging(false);
         }, 100);
       }
     } catch (e) {
       // Handle any server or unexpected errors
-      Alert.alert(
-        "Server Error",
-        "Something went wrong. Please try again later"
-      );
+      Alert.alert('Server Error', 'Something went wrong. Please try again later');
     }
   };
-  
-  createObjectWithDefinedProperties(properties) {
-    return Object.fromEntries(
-      Object.entries(properties).filter(([_, value]) => value !== undefined)
-    );
-  }
 
-  updateProfile = async () => {
+  createObjectWithDefinedProperties(properties) {
+    return Object.fromEntries(Object.entries(properties).filter(([_, value]) => value !== undefined));
+  }
+  checkIfMSVDuplicate = async (msv) => {
+    try {
+      const querySnapshot = await getDocs(query(collection(this.DB, 'users'), where('msv', '==', msv)));
+      return !querySnapshot.empty;
+    } catch (error) {
+      console.error('Error checking for duplicate MSV:', error);
+      return true; // Trả về true để tránh cập nhật khi có lỗi xảy ra
+    }
+  };
+  checkIfEmailExists = async (email) => {
+    try {
+      const querySnapshot = await getDocs(query(collection(this.DB, 'users'), where('email', '==', email)));
+      return !querySnapshot.empty;
+    } catch (error) {
+      console.error('Error checking for existing email:', error);
+      return true; // Trả về true để tránh cập nhật khi có lỗi xảy ra
+    }
+  };
+  updateProfileStudent = async () => {
+    try {
+      const { email, name, msv, classes } = authStore.updateInfo;
+
+      // Kiểm tra xem dữ liệu đã thay đổi so với dữ liệu hiện tại không
+      const isDataChanged =
+        email !== authStore.user.email ||
+        name !== authStore.user.name ||
+        msv !== authStore.user.msv ||
+        classes !== authStore.user.classes;
+
+      if (!isDataChanged) {
+        // Không có sự thay đổi, không cần cập nhật
+        return;
+      }
+
+      // Tiến hành kiểm tra trùng lặp email và MSV
+      const isEmailChanged = email !== authStore.user.email;
+      const isMSVChanged = msv !== authStore.user.msv;
+
+      if (
+        (isEmailChanged && (await this.checkIfEmailExists(email))) ||
+        (isMSVChanged && (await this.checkIfMSVDuplicate(msv)))
+      ) {
+        if (isEmailChanged) {
+          Alert.alert('Duplicate Error', 'Email already exists, please use a different one');
+        } else {
+          Alert.alert('Duplicate Error', 'Student ID already exists, please use a different one');
+        }
+      } else {
+        const updateData = this.createObjectWithDefinedProperties({
+          email,
+          name,
+          classes,
+        });
+
+        await updateDoc(doc(this.DB, 'users', this.user.id), updateData);
+
+        // Đăng xuất nếu email thay đổi, không đăng xuất nếu chỉ cập nhật thông tin khác
+        if (isEmailChanged) {
+          this.logout(true);
+        } else {
+          this.setUpdateInfo({});
+        }
+        Alert.alert('Success', 'Profile updated successfully');
+      }
+    } catch (e) {
+      Alert.alert('Error', 'Something went wrong');
+    }
+  };
+  updateProfileTeacher = async () => {
     try {
       const { email, name, msv, classes } = authStore.updateInfo;
       const updateF = this.createObjectWithDefinedProperties({
@@ -209,41 +221,76 @@ class AuthStore {
         msv,
         classes,
       });
-      await updateDoc(doc(this.DB, "users", this.user.id), updateF);
+      const isEmailExists = await this.checkIfEmailExists(email);
+      if (!isEmailExists) {
+        await updateDoc(doc(this.DB, 'users', this.user.id), updateF);
+        if (email) {
+          this.logout(true);
+        } else {
+          this.logout();
+        }
+      } else {
+        if (isEmailExists) {
+          Alert.alert('Duplicate Error', 'Email already exists, please use a different one');
+        }
+      }
+    } catch (e) {
+      Alert.alert('Error', 'Something error try later');
+    }
+    this.setUpdateInfo({});
+  };
+
+  updateProfile = async () => {
+    try {
+      const { email, name, classes } = authStore.updateInfo;
+      const updateF = this.createObjectWithDefinedProperties({
+        email,
+        name,
+        classes,
+      });
+      await updateDoc(doc(this.DB, 'users', this.user.id), updateF);
       if (email) {
         this.logout(true);
       } else {
         this.logout();
       }
     } catch (e) {
-      Alert.alert("Error", "Something error try later");
+      Alert.alert('Error', 'Something error try later');
     }
     this.setUpdateInfo({});
   };
 
   uploadAvatar = async (uri) => {
-    const url = await fetch(uri);
-    const blob = await url.blob();
-    const storageRef = ref(storage, "/uploads");
-    await uploadBytes(storageRef, blob);
-    const downloadUrl = await getDownloadURL(storageRef);
-    await updateDoc(doc(this.DB, "users", this.user.id), {
-      avatar: downloadUrl,
-    });
-    this.logout();
+    try {
+      const url = await fetch(uri);
+      const blob = await url.blob();
+      const uniqueFileName = `${this.user.id}`;
+      const storageRef = ref(storage, `/uploads/${uniqueFileName}`);
+      // const storageRef = ref(storage, '/uploads');
+      await uploadBytes(storageRef, blob);
+      const downloadUrl = await getDownloadURL(storageRef);
+      await updateDoc(doc(this.DB, 'users', this.user.id), {
+        avatar: downloadUrl,
+      });
+      this.logout();
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   getByName = async (name) => {
-    const rs = await getDocs(
-      query(collection(this.DB, "users"), where("name", "==", name))
-    );
-    let user = null;
-    rs.forEach((doc) => {
-      if (doc.exists()) {
-        user = doc.data();
-      }
-    });
-    return user;
+    try {
+      const rs = await getDocs(query(collection(this.DB, 'users'), where('name', '==', name)));
+      let user = null;
+      rs.forEach((doc) => {
+        if (doc.exists()) {
+          user = doc.data();
+        }
+      });
+      return user;
+    } catch (e) {
+      console.log(e);
+    }
   };
 }
 const authStore = new AuthStore();
--- src/store/ScheduleStore.js
+++ src/store/ScheduleStore.js
@@ -1,23 +1,19 @@
-import {
-  addDoc,
-  collection,
-  doc,
-  getDocs,
-  onSnapshot,
-  query,
-  where,
-} from "firebase/firestore";
-import { fireStore } from "../../firebase.config";
-import { Alert } from "react-native";
-import authStore from "./AuthStore";
+import { addDoc, collection, getDocs, onSnapshot, query, where, orderBy } from 'firebase/firestore';
+import { fireStore } from '../../firebase.config';
+import { Alert } from 'react-native';
+import authStore from './AuthStore';
 
-const { makeAutoObservable } = require("mobx");
+const { makeAutoObservable } = require('mobx');
 
 class ScheduleStore {
   DB = fireStore;
   schedules = [];
+  sc = [];
+  sce = [];
+  ubr = [];
   classes = [];
   notifications = [];
+  subject = [];
 
   constructor() {
     makeAutoObservable(this);
@@ -25,17 +21,112 @@ class ScheduleStore {
   setSchedule = (val) => {
     this.schedules = val;
   };
+  setSc = (val) => {
+    this.sc = val;
+  };
+  setNoti = (val) => {
+    this.notifications = val;
+  };
+  setSce = (val) => {
+    this.sce = val;
+  };
+  setSubject = (val) => {
+    this.subject = val;
+  };
+  setUBR = (val) => {
+    this.ubr = val;
+  };
   setClasses = (val) => {
     this.classes = val;
   };
   getClasses = async () => {
-    const rs = await getDocs(query(collection(this.DB, "classes")));
-    let rss = [];
-    rs.forEach((doc) => {
-      rss.push(doc.data());
-    });
-    this.setClasses(rss);
+    try {
+      const rs = await getDocs(query(collection(this.DB, 'classes')));
+      // console.log('rs::' + JSON.stringify(rs));
+      let rss = [];
+      rs.forEach((doc) => {
+        rss.push(doc.data());
+      });
+      this.setClasses(rss);
+    } catch (e) {
+      console.log(e);
+    }
   };
+  getSubject = async () => {
+    try {
+      const sj = await getDocs(query(collection(this.DB, 'subject')));
+      // console.log("sj::"+sj)
+      let sj1 = [];
+      sj.forEach((doc) => {
+        sj1.push(doc.data());
+      });
+      this.setSubject(sj1);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  getSchedules = async () => {
+    try {
+      const sd = await getDocs(query(collection(this.DB, 'schedules'), orderBy('checked_at', 'asc')));
+      // console.log("sj::"+sj)
+      let sd1 = [];
+      sd.forEach((doc) => {
+        sd1.push(doc.data());
+      });
+      this.setSce(sd1);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getUseByRole = async () => {
+    try {
+      const q = query(collection(this.DB, 'users'), where('role', '==', 'STUDENT'), orderBy('name', 'desc'));
+      const unsub = onSnapshot(q, (querySnapshot) => {
+        const newSchedule = [];
+        querySnapshot.forEach((doc) => {
+          const scheduleData = doc.data();
+          const newUsers = {
+            avatar: scheduleData.avatar,
+            user: scheduleData.name,
+            class: scheduleData.classes.name,
+            email: scheduleData.email,
+            studentID: scheduleData.msv,
+          };
+          newSchedule.push(newUsers);
+        });
+        this.setUBR(newSchedule);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  listenSc = async () => {
+    try {
+      const q = query(collection(this.DB, 'schedules'), orderBy('checked_at', 'asc'));
+      const unsub = onSnapshot(q, (querySnapshot) => {
+        let schedule = [];
+        querySnapshot.docChanges().forEach((change) => {
+          const scheduleData = change.doc.data();
+          // console.log(scheduleData)
+          const selectedFields = {
+            checked_at: new Date(scheduleData.checked_at.seconds * 1000),
+            class: scheduleData.classes.name,
+            subject: scheduleData.subject.name,
+            teacher_id: scheduleData.teacher_id,
+            name: scheduleData.user.name,
+            ID: scheduleData.user.msv,
+          };
+          schedule.push(selectedFields);
+        });
+        this.setSc(schedule);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   listenSchedule = async (teacherId) => {
     try {
       let start = new Date();
@@ -44,17 +135,17 @@ class ScheduleStore {
       let end = new Date();
       end.setUTCHours(23, 59, 59, 999);
       const q = query(
-        collection(this.DB, "schedules"),
-        where("checked_at", ">=", start),
-        where("checked_at", "<=", end),
-        where("teacher_id", "==", teacherId)
+        collection(this.DB, 'schedules'),
+        where('checked_at', '>=', start),
+        where('checked_at', '<=', end),
+        where('teacher_id', '==', teacherId),
       );
 
       const unsub = onSnapshot(q, (querySnapshot) => {
         let schedule = [];
         let mine = false;
         querySnapshot.docChanges().forEach((change) => {
-          if (change.type == "added") {
+          if (change.type == 'added') {
             if (querySnapshot.docChanges().length == 1) {
               if (change.doc.data().teacher_id == authStore.user.id) {
                 mine = true;
@@ -77,39 +168,38 @@ class ScheduleStore {
       console.log(e);
     }
   };
-  setNoti = (val) => {
-    this.notifications = val;
-  };
+
   checked = async (checkData) => {
     try {
       console.log(checkData);
       let start = new Date();
       start.setUTCHours(0, 0, 0, 0);
-
       let end = new Date();
       end.setUTCHours(23, 59, 59, 999);
       const q = query(
-        collection(this.DB, "schedules"),
-        where("checked_at", ">=", start),
-        where("checked_at", "<=", end),
-        where("teacher_id", "==", checkData.teacher_id)
+        collection(this.DB, 'schedules'),
+        where('checked_at', '>=', start),
+        where('checked_at', '<=', end),
+        where('teacher_id', '==', checkData.teacher_id),
       );
       const rs = await getDocs(q);
       let canAdd = true;
       rs.forEach((doc) => {
         if (doc.exists()) {
           const data = doc.data();
-          if (data?.user.name == checkData.user.name) {
+          // console.log('datadoc:' + JSON.stringify(data));
+          // console.log('checkdata::  ' + JSON.stringify(checkData));
+          if (data?.user.name == checkData.user.name && data?.subject.name == checkData.subject.name) {
             canAdd = false;
           }
         }
       });
       if (!canAdd) {
-        Alert.alert("Invalid", "You checked in!");
+        Alert.alert('Invalid', 'You checked in!');
         return;
       }
-      addDoc(collection(this.DB, "schedules"), checkData);
-      Alert.alert("Success", "Check in success");
+      addDoc(collection(this.DB, 'schedules'), checkData);
+      Alert.alert('Success', 'Check in success');
     } catch (e) {
       console.log(e);
     }
